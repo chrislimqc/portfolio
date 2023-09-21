8ecc841e14454c0246c7d1e436adf692f998fdb5
chris <chrislim.qc@gmail.com>
2023-09-21 17:45:28 +0800

    Added initial "storyline"

---
+++ main.js
@@ -0,0 +1,59 @@
+const clickme = document.getElementById("clickme")
+const narration = document.getElementById("narration")
+const clickmeCounter = [0]
+
+clickme.addEventListener("click", function(){
+    if(clickmeCounter[0] == 0){
+        console.log("hello")
+        alert("HELLO CLICK ME AGAIN")
+        document.body.style.backgroundColor = "lightblue";
+        document.body.style.fontFamily = "Arial"
+        clickmeCounter[0] = 1
+        return
+    }
+    document.body.style.backgroundColor = "lightblue";
+    document.body.style.fontFamily = "Arial"
+    const parent = document.getElementById("center")
+    narration.innerHTML = "I will stop harrasing you with popups and talk to you via this text box from now on :-)"
+
+    parent.removeChild(clickme)
+    alert("GOODJOB")
+    createDescription()
+
+
+
+})
+
+
+function createDescription(){
+    //create description of me
+    const div = document.createElement("div")
+
+
+    const name = document.createElement("ul")
+    name.id = "name"
+    name.innerHTML = "Lim Qing Chuan"
+
+
+    const about = document.createElement("ul")
+    about.id = "about"
+    about.innerHTML = "Year 4 Computer Science student at Nanyang Technological University"
+
+
+    const portfolio = document.createElement("ul")
+    portfolio.id = "portfolio"
+    portfolio.innerHTML = "you're looking at it, to skip to the end click here"
+
+
+    const contact = document.createElement("ul")
+    contact.id = "contact"
+    contact.innerHTML = "email: chrislim.qc@gmail.com"
+
+    div.appendChild(name)
+    div.appendChild(about)
+    div.appendChild(portfolio)
+    div.appendChild(contact)
+    
+
+    document.body.appendChild(div)
+}
